144072a3708decf4655f377afc0d327d633f3c52
Amy Rhee <155721990+amysy07@users.noreply.github.com>
2024-11-11 13:23:08 -1000

    updated index

--- js/script.js
+++ js/script.js
@@ -19,3 +19,19 @@ toggleMenuLinks.forEach((el) => {
   }
 });
 
+// ON SCROLL ANIMATION
+// CHANGE ACTIVE STATE FOR ALL ELEMENTS WITH .oberve-me CLASS
+const myobserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.setAttribute("data-viewstate", "active");
+    } else {
+      entry.target.setAttribute("data-viewstate", "inactive");
+    };   
+  });  
+});
+
+const mytargets = document.querySelectorAll('.observe-me');
+mytargets.forEach((el) => {
+    myobserver.observe(el);
+});
